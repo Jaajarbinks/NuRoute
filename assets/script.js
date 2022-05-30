f34c27b821cf04ea848b82f4c745819393c51177
jdog <thuz3rfr13ndly@gmail.com>
2022-05-30 01:59:08 -0600

    set api var and create new html page

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,64 @@
 //APIS here
-
-
+let googleAPIKEY = "";
+let fitnessCalcAPIKEY="";
 // Moment
 
-
-
-
-
-
-
-//Modal 
+//main page Modal
 class BulmaModal {
-	constructor(selector) {
-		this.elem = document.querySelector(selector)
-		this.close_data()
-	}
-	show() {
-		this.elem.classList.toggle('is-active')
-		this.on_show()
-	}
-	close() {
-		this.elem.classList.toggle('is-active')
-		this.on_close()
-	}
-	close_data() {
-		var modalClose = this.elem.querySelectorAll("[data-bulma-modal='close'], .modal-background")
-		var that = this
-		modalClose.forEach(function(e) {
-			e.addEventListener("click", function() {
-				
-				that.elem.classList.toggle('is-active')
+  constructor(selector) {
+    this.elem = document.querySelector(selector);
+    this.close_data();
+  }
+  show() {
+    this.elem.classList.toggle("is-active");
+    this.on_show();
+  }
+  close() {
+    this.elem.classList.toggle("is-active");
+    this.on_close();
+  }
+  close_data() {
+    var modalClose = this.elem.querySelectorAll(
+      "[data-bulma-modal='close'], .modal-background"
+    );
+    var that = this;
+    modalClose.forEach(function (e) {
+      e.addEventListener("click", function () {
+        that.elem.classList.toggle("is-active");
 
-				var event = new Event('modal:close')
+        var event = new Event("modal:close");
 
-				that.elem.dispatchEvent(event);
-			})
-		})
-	}
-	
-	on_show() {
-		var event = new Event('modal:show')
-	
-		this.elem.dispatchEvent(event);
-	}
-	on_close() {
-		var event = new Event('modal:close')
-	
-		this.elem.dispatchEvent(event);
-	}
-	
-	addEventListener(event, callback) {
-		this.elem.addEventListener(event, callback)
-	}
+        that.elem.dispatchEvent(event);
+      });
+    });
+  }
+
+  on_show() {
+    var event = new Event("modal:show");
+
+    this.elem.dispatchEvent(event);
+  }
+  on_close() {
+    var event = new Event("modal:close");
+
+    this.elem.dispatchEvent(event);
+  }
+
+  addEventListener(event, callback) {
+    this.elem.addEventListener(event, callback);
+  }
 }
-var btn = document.querySelector("#btn1")
-var mdl = new BulmaModal("#myModal")
+var btn = document.querySelector("#btn1");
+var mdl = new BulmaModal("#myModal");
 
 btn.addEventListener("click", function () {
-	mdl.show()
-})
+  mdl.show();
+});
 
-mdl.addEventListener('modal:show', function() {
-	console.log("opened")
-})
+mdl.addEventListener("modal:show", function () {
+  console.log("opened");
+});
 
-mdl.addEventListener("modal:close", function() {
-	console.log("closed")
-})
+mdl.addEventListener("modal:close", function () {
+  console.log("closed");
+});
